186c86a1f12c6e712f9854f56a85e4fa7af730dc
isaacans <isaacans@gmail.com>
2024-04-25 22:47:00 +1200

    Complete grid size change feature

--- code.js
+++ code.js
@@ -1,35 +1,47 @@
 // Create references/variables
 const gridContainer = document.querySelector(".grid-container");
 const btnSize = document.querySelector("button#grid-size");
+let penColor = "rgb(98, 103, 167)";
 
-// add 16 by 16 divs to the grid container by adding 256 div items
-// use for loop to add divs, element created in each loop and assign class
-// use external css to set flexbox and arrange the grid with wrap 
-// flex grow is 1, flex shrink is 0, flex basis 6% to have a row of 16
-for (let i = 0; i < 256; i++) {
-    const gridBox = document.createElement("div");
-    gridBox.setAttribute('class', 'grid-box');
-    gridContainer.appendChild(gridBox);
-}
+// Call default grid
+createGrid(16);
 
-// add event listener to div gridContainer that listens for mouseover
-// this takes advantage of bubbling. 
+// Add event listener to div gridContainer that listens for mouseover, uses bubbling
 gridContainer.addEventListener("mouseover", (e) => {
     let target = e.target;
-    if (target.className == "grid-box") {
+    if (target.className == "grid-cell") {
         highlight(target);
     }
 });
-// set up event listener for button click
+
+// Set up event listener for button clicks
 btnSize.addEventListener("click", gridChangePrompt);
+
+/* Declare function that creates the grid - flexBasis used to cause cells
+ to align by setting flex-basis to a percentage, found by dividing 100 by 
+ number of horizontal cells. Uuse for loop to add divs, element created in 
+ each loop to create unique objects otherwise it's the same object appended 
+ on each for loop. Also assign class and use external css to set flexbox and 
+ wrap to true, flex grow is 1, flex shrink is 0. */
+function createGrid(heightWidth) {
+    const flexBasis = 100/heightWidth;
+    const totalCells = heightWidth * heightWidth;
+    for (let i = 0; i < totalCells; i++) {
+        const gridCell = document.createElement("div");
+        gridCell.setAttribute('class', 'grid-cell');
+        gridCell.style.flexBasis = `${flexBasis}%`;
+        gridContainer.appendChild(gridCell);
+    };
+};
 
 // Declare function to highlight bg-color with element as parameter
 function highlight(elem) {
-    elem.style.backgroundColor = "red";
+    elem.style.backgroundColor = penColor;
 };
 
+// Cancel exits prompt, which only accepts numbers and throws an error for the unexpected. 
 function gridChangePrompt() {
-    input = prompt("Enter a number from 1 to 100. The grid will update with that number of squares on each side.");
+    input = prompt("Enter a number from 1 to 100. The grid will update with that number of vertical & horizontal cells.");
     if (input === null) {
         return;
     } else if (isNaN(input)) {
@@ -39,14 +51,17 @@ function gridChangePrompt() {
         alert("Your number is out of range. Please pick a number from 1 to 100.");
         gridChangePrompt();
     } else if (input >= 1 && input <= 100) {
-        alert(input + " has been accepted!");
-        changeGridSize(input);
+        removeGrid();
+        createGrid(input);
     } else {
         alert("Error, please contact IT support");
     };
     return;
 };
 
-function changeGridSize(heightWidth) {
-    alert("changeGridSize Called");
-}
+function removeGrid() {
+    const oldGridCells = document.querySelectorAll("div.grid-cell");
+    oldGridCells.forEach((gridCell) => {
+        gridCell.remove();
+    });
+};
